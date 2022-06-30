2474333992619efd48905bd043256ade2089e753
qodbtn41 <qodbtn41@naver.com>
2022-06-30 20:04:45 +0900

    ProductCard CSS 수정, hover 등..

--- cafe24-tsb/src/Components/Main/ProductCard.jsx
+++ cafe24-tsb/src/Components/Main/ProductCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProductCard.scss';
+import sampleImage from '../../Resource/Image/ProductCardImageSample01.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const ProductCard = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className='product_card'>
-      <img src='/src/Resource/Image/ProductCardImageSample01.png' width={300} height={100}/>
+    <div className='product_card'
+      onMouseOver={() => setIsHovering(true)}
+      onMouseOut={() => setIsHovering(false)}>
+      <div className='product_image'>
+        <img src={sampleImage} alt='제품 이미지'/>
+        <div className={isHovering?'hover':''}></div>
+      </div>
       <div>
         <div className='product_tags'>
           <div>PC</div>
@@ -15,8 +25,8 @@ const ProductCard = () => {
           <div>베이직_모바일무료</div>
           <div>150,000원</div>
         </div>
-        <div className='product_card_footer'>
-          <i>하트</i>
+        <div className={isHovering?'product_card_footer hover':'product_card_footer'}>
+          <FontAwesomeIcon icon={faHeart}/>
           <div>SAMPLE</div>
         </div>
       </div>
